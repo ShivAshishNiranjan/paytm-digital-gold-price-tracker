6ab74482165f11ede5a2e25d742b6f110eddafe7
Shiv Ashish <shivashish.niranjan@grofers.com>
2020-05-28 01:49:32 +0530

    28th May | Dumping Prices to CSV File

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 __author__ = 'shiv_ashish_niranjan'
+from datetime import date
 
 import requests
+import csv
+import os.path
 
 
 def test_paytm_digital_gold_price():
@@ -15,6 +18,22 @@ def test_paytm_digital_gold_price():
 
     print("Buying Price is {}".format(buy_price_per_gm))
     print("Selling Price is {}".format(sell_price_per_gm))
+
+    csv_columns = ['Date', 'Buy_Price', 'Sell_Price']
+    csv_file = "paytm_digital_gold_price.csv"
+
+    dict_data = {'Date': date.today(), 'Buy_Price': buy_price_per_gm, 'Sell_Price': sell_price_per_gm}
+
+    file_exists = os.path.isfile(csv_file)
+
+    try:
+        with open(csv_file, 'a') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            if not file_exists:
+                writer.writeheader()
+            writer.writerow(dict_data)
+    except IOError:
+        print("I/O error")
 
 
 if __name__ == '__main__':
